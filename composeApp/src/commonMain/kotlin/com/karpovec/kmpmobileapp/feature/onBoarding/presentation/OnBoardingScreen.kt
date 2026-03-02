4127d9d06bd2a98acd688fd7a70b853b7d64a8d3
KarpovetsIlya <164213460+KarpovetsIlya@users.noreply.github.com>
2026-03-02 15:11:01 +0300

    refactor: move login + replace onboarding screen

---
+++ composeApp/src/commonMain/kotlin/com/karpovec/kmpmobileapp/feature/onBoarding/presentation/OnBoardingScreen.kt
@@ -0,0 +1,105 @@
+package com.karpovec.kmpmobileapp.feature.onBoarding.presentation
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.karpovec.kmpmobileapp.feature.onBoarding.presentation.components.OnboardingPageContent
+import com.karpovec.kmpmobileapp.feature.onBoarding.presentation.components.PagerDots
+import com.karpovec.kmpmobileapp.feature.onBoarding.presentation.model.onboardingPages
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun WelcomeScreen(
+    onFinish: () -> Unit
+) {
+    val cs = MaterialTheme.colorScheme
+    val scope = rememberCoroutineScope()
+
+    val pages = remember { onboardingPages() }
+    val pagerState = rememberPagerState(pageCount = { pages.size })
+    val isLastPage = pagerState.currentPage == pages.lastIndex
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = cs.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+        ) {
+
+            Spacer(Modifier.height(48.dp))
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) { page ->
+                OnboardingPageContent(
+                    page = pages[page],
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 72.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                PagerDots(
+                    total = pages.size,
+                    selected = pagerState.currentPage
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        if (isLastPage) {
+                            onFinish()
+                        } else {
+                            scope.launch {
+                                pagerState.animateScrollToPage(
+                                    pagerState.currentPage + 1
+                                )
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.75f)
+                        .height(56.dp),
+                    shape = RoundedCornerShape(18.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = cs.primary,
+                        contentColor = cs.onPrimary
+                    )
+                ) {
+                    Text(
+                        text = if (isLastPage) "Start" else "Next",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
+}
